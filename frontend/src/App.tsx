57e7e6495af28c8873aecb2207aa93b24b67207a
kwabenakay <nskwbn@gmail.com>
2023-05-22 15:07:14 +0000

    Added and styled ther serving page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import Loading from './pages/Loading'
 import Login from './pages/Login'
 import Order from './pages/Order'
+import Serving from './pages/Serving'
 
 function App() {
   return (
     <div className=' w-[1440px] mx-auto'>
-      <Loading/>
+      <Serving/>
     </div>
   )
 }
