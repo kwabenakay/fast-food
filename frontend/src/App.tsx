6093348a4ef46c853218435c458df4370ab1fd15
kwabenakay <nskwbn@gmail.com>
2023-05-22 15:37:15 +0000

    Implemented routing of application in URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
-import Loading from './pages/Loading'
-import Login from './pages/Login'
-import Order from './pages/Order'
-import Serving from './pages/Serving'
+import Loading from "./pages/Loading";
+import Login from "./pages/Login";
+import Order from "./pages/Order";
+import Serving from "./pages/Serving";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className=' w-[1440px] mx-auto'>
-      <Serving/>
+    <div className=" w-[1440px] mx-auto">
+      <Routes>
+        <Route path="/" element={<Navigate to={"/login"} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/seerving" element={<Serving />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
