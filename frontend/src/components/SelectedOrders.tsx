1f6ec3a0b6a0704c3bda87e3d122bbc489e361e1
kwabenakay <nskwbn@gmail.com>
2023-05-23 15:06:51 +0000

    Implemented the remove feature of the selectedItems

--- frontend/src/components/SelectedOrders.tsx
+++ frontend/src/components/SelectedOrders.tsx
@@ -1,36 +1,47 @@
+import {useEffect} from "react";
 import CloseIcon from "@mui/icons-material/Close";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { removeSelect } from "../redux/selectedItems";
 
 export default function SelectedOrders() {
-  const orders:{ name: string; num: number }[]= useSelector((state:any)=>state.selected)
-  console.log(orders)
+  const orders: { name: string; num: number }[] = useSelector(
+    (state: any) => state.selected
+  );
+  const dispatch = useDispatch();
+  useEffect(()=>{},[orders])
   return (
     <div className=" bg-black bg-opacity-10 h-56 rounded-lg overflow-y-auto grid grid-cols-2 gap-2 p-4">
-      {orders && orders.map((order, ind) => (
-        <div
-          id={"tag" + ind}
-          key={"order" + ind}
-          className=" bg-[#E1A272] h-12 p-1 flex justify-between items-center rounded-sm text-3xl"
-        >
-          {order.name + " " + order.num}
-          <span
-            onMouseOver={() => {
-              let tag = document.getElementById("tag" + ind);
-              if (tag) {
-                tag.style.backgroundColor = "#FF8D8D";
-              }
-            }}
-            onMouseOut={() => {
-              let tag = document.getElementById("tag" + ind);
-              if (tag) {
-                tag.style.backgroundColor = "#E1A272";
-              }
-            }}
+      {orders &&
+        orders.map((order, ind) => (
+          <div
+            id={"tag" + ind}
+            key={"order" + ind}
+            className=" bg-[#E1A272] h-12 p-1 flex justify-between items-center rounded-sm text-3xl"
           >
-            <CloseIcon htmlColor="#FE0404" fontSize="large" />
-          </span>
-        </div>
-      ))}
+            {order.name + " " + order.num}
+            <span
+              onMouseOver={() => {
+                let tag = document.getElementById("tag" + ind);
+                if (tag) {
+                  tag.style.backgroundColor = "#FF8D8D";
+                }
+              }}
+              onMouseOut={() => {
+                let tag = document.getElementById("tag" + ind);
+                if (tag) {
+                  tag.style.backgroundColor = "#E1A272";
+                }
+              }}
+              onClick={() => {
+                console.log(orders.indexOf(order))
+                dispatch(removeSelect(orders.indexOf(order)));
+                console.log(order.name);
+              }}
+            >
+              <CloseIcon htmlColor="#FE0404" fontSize="large" />
+            </span>
+          </div>
+        ))}
     </div>
   );
 }
--- frontend/src/redux/selectedItems.ts
+++ frontend/src/redux/selectedItems.ts
@@ -1,44 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-type selectedItems = { name: string; num: number; price:number }[];
+type selectedItems = { name: string; num: number; price: number }[];
 const selectedItems = [
-  { name: "Malt", num: 10, price:10 },
-  { name: "Malt", num: 10, price:10 },
-  { name: "Fanta", num: 10, price:10 },
-  { name: "Malt", num: 10, price:10 },
-  { name: "Malt", num: 10, price:10 },
-  { name: "Malt", num: 10, price:10 },
-  { name: "Malt", num: 10, price:10 },
-  { name: "Malt", num: 10, price:10 },
-  { name: "Malt", num: 10, price:10 },
-  { name: "Malt", num: 10, price:10 },
+  { name: "Malt", num: 10, price: 10 },
+  { name: "Sprite", num: 10, price: 10 },
+  { name: "Fanta", num: 10, price: 10 },
+  { name: "Coke", num: 10, price: 10 },
+  { name: "Stars", num: 10, price: 10 },
+  { name: "Moon", num: 10, price: 10 },
+  { name: "Sky", num: 10, price: 10 },
+  { name: "Plane", num: 10, price: 10 },
+  { name: "Car", num: 10, price: 10 },
+  { name: "Bike", num: 10, price: 10 },
 ];
 
 export const SELECTED_ITEMS = createSlice({
   name: "seletedItems",
   initialState: selectedItems,
   reducers: {
-    addSelect: (state,actions) => {
-     selectedItems.push( actions.payload.selected)
-     return state
-    },
-    removeSelect: (state, actions) => {
-      let item = actions.payload.item;
-      let index = -1;
-      state.filter((val, ind) => {
-        if (val.name === item.name) index = ind;
-        return val.name === item.name;
-      });
-      state.splice(index,1)
+    addSelect: (state, actions) => {
+      selectedItems.push(actions.payload.selected);
       return state;
     },
-    clearSelect:(state)=>{
-      state=[]
-      return state
-    }
+    removeSelect: (state, actions) => {
+      
+      state.splice(actions.payload, 1);
+      return state;
+    },
+    clearSelect: (state) => {
+      state = [];
+      return state;
+    },
   },
 });
 
-export const { addSelect, removeSelect,clearSelect } = SELECTED_ITEMS.actions;
+export const { addSelect, removeSelect, clearSelect } = SELECTED_ITEMS.actions;
 export default SELECTED_ITEMS.reducer;
